0dd7fe6cb681b36294b97dba6fe48c60f6982731
Swaraj Puppalwar <79976106+UltronTheAI@users.noreply.github.com>
2023-02-18 10:26:12 +0530

    Add files via upload

---
+++ contact/js/main.js
@@ -0,0 +1,27 @@
+async function post() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var text = document.getElementById('text').value;
+    fetch(`https://formspree.io/f/myyawyvy`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+            text: text
+        })
+    })
+    .then(response => {
+        fetch(`./result/index.html`)
+        .then(response => response.text())
+        .then(data => {
+            if (response.ok) {
+                document.documentElement.innerHTML = data.replace('@$title', 'Email sended!').replace('@$text', 'Your email was sended sucessfully, we will reply you with in 2 days, with our email `epicdeveloper14@gmail.com`!')
+            } else {
+                document.documentElement.innerHTML = data.replace('@$title', 'Failed to send email!').replace('@$text', 'Your email was not sended sucessfully, try again!')
+            }
+        })
+    })
+}
---
+++ js/main.js
@@ -0,0 +1,33 @@
+function managePosts(posts) {
+    var blog = document.querySelector('.blogs')
+    let n = 0
+    posts.forEach(post => {
+        n += 1
+        localStorage.setItem(`post${String(n)}`, JSON.stringify(post))
+        blog.innerHTML += '<div id="' + String(n) + '" class="hover:translate-y-1" onclick="openPost(' + String(n) + ')">\
+        <div class="overflow-hidden rounded-md bg-slate-800">\
+          <div class="image aspect-w-3 aspect-h-2"><img class="h-full w-full object-cover object-center"\
+              src="' + post.image + '" alt="Image post" loading="lazy" /></div>\
+          <div class="px-3 pt-4 pb-6 text-center">\
+            <h2 class="tittle text-xl font-semibold">' + post.title + '</h2>\
+            <div class="date mt-1 text-xs text-gray-400">' + post.date + '</div>\
+            <div class="snippet mt-2 text-sm">' + post.snippet + '</div>\
+          </div>\
+        </div>\
+        </div>'
+    });
+} function load () {
+    var url = './data/projects.json'
+    fetch(url).then(data => {
+        return data.json()
+    }).then(objectData => {
+        managePosts(objectData.projects.slice(0, 3))
+    })
+} function openPost(id) {
+  var post = JSON.parse(localStorage.getItem(`post${id}`))
+  fetch('/posts/post.html')
+  .then(response => response.text())
+  .then(data => {
+    document.documentElement.innerHTML = data.replace('#title', post.title).replace('#title', post.title).replace('$@image', post.image).replace('$@body', post.body).replace('$@date', post.date)
+  })
+}
